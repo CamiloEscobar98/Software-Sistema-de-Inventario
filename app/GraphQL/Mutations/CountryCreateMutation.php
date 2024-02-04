9a07c4a81c039a4b0e738dfcf054dbab0d88eb86
Andrés Yáñez <camilo_escobar2398@outlook.com>
2024-02-03 22:21:04 -0500

    Faltaba el locale para el lenguaje

--- app/GraphQL/Mutations/CountryCreateMutation.php
+++ app/GraphQL/Mutations/CountryCreateMutation.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\GraphQL\Mutations;
 
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\App;
 use Illuminate\Database\QueryException;
 
 use GraphQL\Type\Definition\ResolveInfo;
@@ -18,6 +19,7 @@ use App\Repositories\CountryRepository;
 use App\Factories\CountryFactory;
 
 use App\Enums\CountryEnum;
+use App\Enums\LanguageEnum;
 
 use Closure;
 
@@ -43,6 +45,10 @@ class CountryCreateMutation extends Mutation
             CountryEnum::Slug => [
                 'type' => Type::string(),
                 'description' => 'Please, type the Country`s slug.'
+            ],
+            LanguageEnum::Locale => [
+                'type' => Type::string(),
+                'description' => 'Language'
             ]
         ];
     }
@@ -52,13 +58,15 @@ class CountryCreateMutation extends Mutation
         $countryTable = CountryEnum::Table;
         $countrySlugColumn = CountryEnum::Slug;
         return [
-            CountryEnum::Name => ['required', 'string'],
-            CountryEnum::Slug => ['required']
+            CountryEnum::Name => ['required', "string", "max:100"],
+            CountryEnum::Slug => ['required', "string", "max:50"]
         ];
     }
 
     public function resolve($root, array $args, $context, ResolveInfo $resolveInfo, Closure $getSelectFields, CountryRepository $countryRepository)
     {
+        App::setLocale($args[LanguageEnum::Locale] ?? App::getLocale());
+        
         try {
             $country = $countryRepository->create(CountryFactory::create(null, $args[CountryEnum::Name], $args[CountryEnum::Slug]));
             return $country;
