36e8bf0d29ede657930edee5da5cc91b4ff05640
Andrés Yáñez <camilo_escobar2398@outlook.com>
2024-11-02 20:57:31 -0500

    Listo finalizado

--- resources/views/livewire/profile/delete-user-form.blade.php
+++ resources/views/livewire/profile/delete-user-form.blade.php
@@ -30,7 +30,7 @@ new class extends Component
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            {{ __('Delete Account Text') }}
         </p>
     </header>
 
@@ -43,11 +43,11 @@ new class extends Component
         <form wire:submit="deleteUser" class="p-6">
 
             <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{ __('Are you sure you want to delete your account?') }}
+                {{ __('Delete Account Ask') }}
             </h2>
 
             <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                {{ __('Delete Account Text') }}
             </p>
 
             <div class="mt-6">
